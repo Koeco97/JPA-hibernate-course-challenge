01a2e9fdf0016b1a2999d07b6f6345a7d8f4fc69
Koeco97 <koeco97@gmail.com>
2024-12-31 11:31:49 +0100

    Part3

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -7,17 +7,57 @@ import jakarta.persistence.Persistence;
 
 public class Main {
   public static void main(String[] args) {
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
-      try (emf) {
-          EntityManager em = emf.createEntityManager(); // Represent the persistence context
-
-          em.getTransaction().begin();
-
-          Student student = new Student();
-          student.setName("John");
-          em.persist(student);
-
-          em.getTransaction().commit();
+      try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit")) {
+          // create(emf);
+          // update(emf);
+          // attachAndDetach(emf);
+          remove(emf);
       }
   }
+
+    private static void create(EntityManagerFactory emf){
+        EntityManager em = emf.createEntityManager(); // Represents the persistence context
+        try (em) {
+            em.getTransaction().begin();
+
+            Student student = new Student();
+            student.setName("John");
+            em.persist(student);
+
+            em.getTransaction().commit();
+        }
+    }
+
+    private static void update(EntityManagerFactory emf){
+        EntityManager em = emf.createEntityManager();
+        try (em) {
+            em.getTransaction().begin();
+            Student student = em.find(Student.class, 1L);  // 1L - primary key
+            student.setName("Peter");
+            em.getTransaction().commit();
+        }
+    }
+
+    private static void attachAndDetach(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+        try (em) {
+            em.getTransaction().begin();
+            Student student = new Student();
+            student.setName("Mary");
+            // em.merge(student);
+            em.detach(student);
+            student.setName("Alice");
+            em.getTransaction().commit();
+        }
+    }
+
+    private static void remove(EntityManagerFactory emf){
+      EntityManager em = emf.createEntityManager();
+      try (em) {
+          em.getTransaction().begin();
+          Student student = em.find(Student.class, 1L);
+          em.remove(student);
+          em.getTransaction().commit();
+      }
+    }
 }
